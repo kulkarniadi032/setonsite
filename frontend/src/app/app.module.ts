632855c198939e016d44d01a30d4c94b3d6b63ba
Aditya Kulkarni <kulkarniadi032@gmail.com>
2022-03-20 10:17:44 +0530

    page4 created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { Page2Component } from './pages/page2/page2.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './mat-module.module';
 import { Page3Component } from './pages/page3/page3.component';
+import { Page4Component } from './pages/page4/page4.component';
 
 @NgModule({
-  declarations: [AppComponent, Page1Component, Page2Component, Page3Component],
+  declarations: [AppComponent, Page1Component, Page2Component, Page3Component, Page4Component],
   imports: [
     BrowserModule,
     AppRoutingModule,
