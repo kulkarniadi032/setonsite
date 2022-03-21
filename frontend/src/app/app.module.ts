942f6a65de51b54b9e0c733c06b59b18fa492310
Aditya Kulkarni <kulkarniadi032@gmail.com>
2022-03-21 22:19:15 +0530

    trial branch

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,9 +14,10 @@ import { Page4Component } from './pages/page4/page4.component';
 import { Page5Component } from './pages/page5/page5.component';
 import { TestsComponent } from './pages/tests/tests.component';
 import { Page6Component } from './pages/page6/page6.component';
+import { Page9Component } from './pages/page9/page9.component';
 
 @NgModule({
-  declarations: [AppComponent, Page1Component, Page2Component, Page3Component, Page4Component, Page5Component, TestsComponent, Page6Component],
+  declarations: [AppComponent, Page1Component, Page2Component, Page3Component, Page4Component, Page5Component, TestsComponent, Page6Component, Page9Component],
   imports: [
     BrowserModule,
     AppRoutingModule,
