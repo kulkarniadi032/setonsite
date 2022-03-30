5592326bd508f7f5cc954353ff91c7aeb050ef98
Aditya Kulkarni <kulkarniadi032@gmail.com>
2022-03-30 16:28:28 +0530

    page4 pi value calculation done

--- frontend/src/app/pages/page4/page4.component.ts
+++ frontend/src/app/pages/page4/page4.component.ts
@@ -1,15 +1,55 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page4',
   templateUrl: './page4.component.html',
-  styleUrls: ['./page4.component.css']
+  styleUrls: ['./page4.component.css'],
 })
 export class Page4Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  form4 = new FormGroup({
+    HVE15: new FormControl('', [Validators.required]),
+    LVE15: new FormControl('', [Validators.required]),
+    HVLVE15: new FormControl('', [Validators.required]),
+    HVE60: new FormControl('', [Validators.required]),
+    LVE60: new FormControl('', [Validators.required]),
+    HVLV60: new FormControl('', [Validators.required]),
+    HVE600: new FormControl('', [Validators.required]),
+    LVE600: new FormControl('', [Validators.required]),
+    HVLV600: new FormControl('', [Validators.required]),
+  });
+  createPage4Data(data: any) {
+    console.log(data);
   }
-
+  HVE15: any;
+  HVE60: any;
+  handleHVE15(data: any) {
+    console.log(data);
+    this.HVE15 = data;
+    this.PI15();
+  }
+  handleLVE15(data: any) {
+    console.log(data);
+  }
+  handleHVLVE15(data: any) {
+    console.log(data);
+  }
+  handleHVE60(data: any) {
+    console.log(data);
+    this.HVE60 = data;
+    this.PI15();
+  }
+  handleLVE60(data: any) {
+    console.log(data);
+  }
+  handleHVLV60(data: any) {
+    console.log(data);
+  }
+  PI15Data: any;
+  PI15() {
+    this.PI15Data = this.HVE15 / this.HVE60;
+  }
 }
